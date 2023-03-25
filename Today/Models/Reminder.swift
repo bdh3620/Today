f152680218a43dc5ab0408829223478aec9105e1
백대홍 <bdh3620@naver.com>
2023-03-26 01:03:07 +0900

    first

---
+++ Today/Models/Reminder.swift
@@ -0,0 +1,8 @@
+//
+//  Reminder.swift
+//  Today
+//
+//  Created by 백대홍 on 2023/03/26.
+//
+
+import Foundation
---
+++ Today/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Today
+//
+//  Created by 백대홍 on 2023/03/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
